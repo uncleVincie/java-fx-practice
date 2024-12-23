3558dfb3afe02a2963f4b61e07eeb247745a457f
Vincent Mariucci <mariucci1@gmail.com>
2024-12-23 15:58:22 -0500

    adding css

--- src/main/java/com/vincie/HelloFX.java
+++ src/main/java/com/vincie/HelloFX.java
@@ -1,8 +1,11 @@
 package com.vincie;
 
 import javafx.application.Application;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -10,10 +13,14 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class HelloFX extends Application {
+
+    private final StringProperty greeting = new SimpleStringProperty("");
+    private final StringProperty name = new SimpleStringProperty("");
 
     @Override
     public void start(Stage stage) {
@@ -22,22 +29,38 @@ public class HelloFX extends Application {
         stage.show();
     }
 
-    private Parent createContent() {
-        TextField inputTextField = new TextField("");
-        HBox inputRow = new HBox(new Label("Name:"), inputTextField);
-        inputRow.setSpacing(6);
-        inputRow.setAlignment(Pos.CENTER);
-        Label outputLabel = new Label("");
-        Button actionButton = new Button("Hello");
-        actionButton.setOnAction(event -> outputLabel.setText("Hello " + inputTextField.getText()));
-        inputTextField.setOnKeyPressed(event -> {
-           if (event.getCode() == KeyCode.ENTER) {
-               outputLabel.setText("Hello " + inputTextField.getText());
-           }
-        });
-        VBox results = new VBox(20, inputRow, outputLabel, actionButton);
+    private Region createContent() {
+        VBox results = new VBox(20, createInputRow(), createOutputLabel(), createGreetingButton());
         results.setAlignment(Pos.CENTER);
+        results.getStylesheets().add(this.getClass().getResource("/css/beginners.css").toExternalForm());
         return results;
+    }
+
+    private Node createGreetingButton() {
+        Button results = new Button("Hello");
+        results.setOnAction(evt -> setGreeting());
+        return results;
+    }
+
+    private Node createInputRow() {
+        TextField textField = new TextField("");
+        textField.textProperty().bindBidirectional(name);
+        Label namePrompt = new Label("Name:");
+        namePrompt.getStyleClass().add("prompt-label");
+        HBox hBox = new HBox(6, namePrompt, textField);
+        hBox.setAlignment(Pos.CENTER);
+        return hBox;
+    }
+
+    private Node createOutputLabel() {
+        Label results = new Label("");
+        results.getStyleClass().add("greeting-label");
+        results.textProperty().bind(greeting);
+        return results;
+    }
+
+    private void setGreeting() {
+        greeting.set("Hello " + name.get());
     }
 
     public static void main(String[] args) {
